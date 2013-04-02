2a80da9852cb91e367324d7593b9b7b8b3343bfe
brandonschlack <ilikemuffins58@gmail.com>
2013-04-02 03:49:05 -0700

    Playlist Activity displays a list

--- MyPace/src/cs/pacificu/mypace/MainActivity.java
+++ MyPace/src/cs/pacificu/mypace/MainActivity.java
@@ -2,24 +2,36 @@ package cs.pacificu.mypace;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 //Viewed by Ryan Newman
 //Viewed by Brandon Schlack
 public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public void startPlaylists (View view)
+    {
+    	Intent intentPlaylists = new Intent();
+    	intentPlaylists.setClassName("cs.pacificu.mypace", "cs.pacificu.mypace.Playlist");
+    	intentPlaylists.setAction("@strings/action_playlists");
+    	startActivity(intentPlaylists);
     }
     
 }
--- MyPace/src/cs/pacificu/mypace/Playlist.java
+++ MyPace/src/cs/pacificu/mypace/Playlist.java
@@ -1,10 +1,45 @@
+/*
+ * Displays a list.
+ * Used following URL for tutorial
+ * 
+ * http://www.mkyong.com/android/android-listview-example/
+ * 
+ * Brandon Schlack. 04/02/2013 03:46am
+ */
 package cs.pacificu.mypace;
 
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.app.ListActivity;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Playlist extends ListActivity
 {
+	static final String[] PLAYLISTS = new String[] {"Best Running Songs", 
+		"Best Walking Songs", "Personal Playlist"};
 	
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		//setContentView(R.layout.playlists);
+		
+		setListAdapter(new ArrayAdapter<String>(this, R.layout.playlists,PLAYLISTS));
+		
+		ListView listView = getListView();
+		listView.setTextFilterEnabled(true);
+		listView.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+			    // When clicked, show a toast with the TextView text
+			    Toast.makeText(getApplicationContext(),
+				((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
 }
