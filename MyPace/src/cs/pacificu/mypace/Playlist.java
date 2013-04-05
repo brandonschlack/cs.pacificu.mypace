f707e9cf886e0fd826967e03deb850f0c54acafe
brandonschlack <ilikemuffins58@gmail.com>
2013-04-05 13:20:47 -0700

    Playlist list. Now working on Toast Notif method.

--- MyPace/src/cs/pacificu/mypace/Playlist.java
+++ MyPace/src/cs/pacificu/mypace/Playlist.java
@@ -33,9 +33,11 @@ public class Playlist extends ListActivity
 		
 		ListView listView = getListView();
 		listView.setTextFilterEnabled(true);
-		listView.setOnItemClickListener(new OnItemClickListener() {
+		listView.setOnItemClickListener(new OnItemClickListener()
+		{
 			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
+					int position, long id)
+			{
 			    // When clicked, show a toast with the TextView text
 			    Toast.makeText(getApplicationContext(),
 				((TextView) view).getText(), Toast.LENGTH_SHORT).show();
