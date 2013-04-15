56d90d2d9df3fe91886071bf60cf28890aedbb3c
brandonschlack <ilikemuffins58@gmail.com>
2013-04-14 22:25:04 -0700

    Hard merge of Liam's code

--- MyPace/gen/cs/pacificu/mypace/R.java
+++ MyPace/gen/cs/pacificu/mypace/R.java
@@ -29,17 +29,21 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090003;
+        public static final int action_settings=0x7f090006;
         public static final int buttonPlaylists=0x7f090000;
-        public static final int playlistItem=0x7f090002;
-        public static final int playlists=0x7f090001;
-        public static final int skin=0x7f090004;
+        public static final int edit_name=0x7f090001;
+        public static final int lbl_your_name=0x7f090002;
+        public static final int playlistItem=0x7f090005;
+        public static final int playlists=0x7f090004;
+        public static final int skin=0x7f090007;
+        public static final int txt_your_name=0x7f090003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int playlist=0x7f030001;
-        public static final int settings=0x7f030002;
-        public static final int single_list_item=0x7f030003;
+        public static final int fragment_edit_pace=0x7f030001;
+        public static final int playlist=0x7f030002;
+        public static final int settings=0x7f030003;
+        public static final int single_list_item=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f080000;
--- MyPace/src/cs/pacificu/mypace/Settings.java
+++ MyPace/src/cs/pacificu/mypace/Settings.java
@@ -1,7 +1,15 @@
 package cs.pacificu.mypace;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
 
 
 public class Settings extends PreferenceActivity
@@ -16,6 +24,65 @@ public class Settings extends PreferenceActivity
 		
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.layout.settings);
+		Preference equalizerPref = (Preference) findPreference ("equalizer");
+		equalizerPref.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+			@Override
+			public boolean onPreferenceClick (Preference preference) {
+				// TODO: laters
+				return false;
+			}
+			
+		});
+		Preference pacePref = (Preference) findPreference ("pace");
+		pacePref.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+			@Override
+			public boolean onPreferenceClick (Preference preference) {
+				/*AlertDialog paceDialog = new AlertDialog.Builder(Settings.this).create();
+				
+				final TextView minutesseconds = new TextView(Settings.this);
+				minutesseconds.setText("450");
+				
+				SeekBar seekBar = new SeekBar(Settings.this);
+		        seekBar.setMax(900);
+
+		        seekBar.setProgress(450);
+		        seekBar.setVisibility(View.VISIBLE);
+		        seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+		        	@Override
+		        	public void onProgressChanged (SeekBar seekBar, int progress, boolean fromUser)
+		        	{
+		        		//minutesseconds.setText(Integer.toString(progress));
+		        	}
+		        	
+		        	public void onStopTrackingTouch (SeekBar seekBar)
+		        	{
+		        		//nothing
+		        	}
+		        	
+		        	public void onStartTrackingTouch (SeekBar seekBar)
+		        	{
+		        		//nothing
+		        	}
+		        });
+		        
+				paceDialog.setTitle(R.string.settings_Pace);
+				paceDialog.setMessage("Determines what pace is running");
+				paceDialog.setView(seekBar);
+				paceDialog.setButton("OK", new DialogInterface.OnClickListener() {
+				      public void onClick(DialogInterface dialog, int which) {
+				 
+				       //here you can add functions
+				 
+				    } }); 
+				paceDialog.show();*/
+				
+				/* old code w/o fragment */
+				
+				return false;
+			}
+			
+		});
 	}
+
 }
 
